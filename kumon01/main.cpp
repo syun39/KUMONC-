6db5faba2fb5feeba821bd6078f4946797240439
syun39 <134475243+syun39@users.noreply.github.com>
2024-11-16 13:19:14 +0900

    C++公文式レベル1

--- kumon01/main.cpp
+++ kumon01/main.cpp
@@ -5,7 +5,19 @@ int main(int argc, char* argv[])
 	for (int i = 1; i <= 100; ++i)
 	{
 		//判定処理をここに書いてください
-		 
+		if (i % 15 == 0) {
+			std::cout << "hogepiyo" << std::endl;
+		 }
+		else if (i % 5 == 0) {
+			std::cout << "piyo" << std::endl;
+		}
+		else if (i % 3 == 0) {
+			std:: cout << "hoge" << std::endl;
+		}
+		else {
+			std::cout << i << std::endl;
+		}
+
 		//出力は以下の形式を使用してください
 		//std::cout << i << std::endl;		//数字を出力する
 		//std::cout << "hoge" << std::endl;	//文字列を出力する
